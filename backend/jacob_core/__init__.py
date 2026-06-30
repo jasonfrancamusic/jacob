f4a119c65b4be8f8033db57a93529509ae0700ff
jasonfrancamusic <jasonfrancamusic@gmail.com>
2026-06-30 13:54:28 -0400

    Add Jacob Core package init

---
+++ backend/jacob_core/__init__.py
@@ -0,0 +1,8 @@
+"""Jacob Core package.
+
+Jacob Core is the first executable nucleus of Jacob 0.1.
+It receives partner input, understands intent, checks memory,
+routes to specialists, responds, reflects and suggests what to learn.
+"""
+
+__version__ = "0.1.0"
