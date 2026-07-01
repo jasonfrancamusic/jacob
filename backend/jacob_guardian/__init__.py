fc8da10cabf9de9f740d8807bd2f4e89edee9b3c
jasonfrancamusic <jasonfrancamusic@gmail.com>
2026-07-01 01:29:35 -0400

    Add Jacob Guardian package

---
+++ backend/jacob_guardian/__init__.py
@@ -0,0 +1,9 @@
+"""Jacob Guardian package.
+
+Guardian is the silent observer layer of Jacob OS.
+It prepares briefings, timeline events and system observations for Jacob Brain.
+"""
+
+from .guardian import JacobGuardian
+
+__all__ = ["JacobGuardian"]
