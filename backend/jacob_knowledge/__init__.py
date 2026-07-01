5b821d9f234c35e2531a17a5f70fe4f82e37aa1b
jasonfrancamusic <jasonfrancamusic@gmail.com>
2026-07-01 05:11:00 -0400

    Add Jacob Knowledge package

---
+++ backend/jacob_knowledge/__init__.py
@@ -0,0 +1,8 @@
+"""Jacob Knowledge Core.
+
+General public knowledge store separated from private partner memory.
+"""
+
+from .knowledge_core import KnowledgeCore, KnowledgeRecord
+
+__all__ = ["KnowledgeCore", "KnowledgeRecord"]
