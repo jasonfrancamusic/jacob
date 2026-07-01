65bb2b39225c2fb14d30f0275fb3bd51ebc1293b
jasonfrancamusic <jasonfrancamusic@gmail.com>
2026-06-30 21:29:18 -0400

    Add Jacob Brain package

---
+++ backend/jacob_brain/__init__.py
@@ -0,0 +1,9 @@
+"""Jacob Brain package.
+
+Jacob Brain sits between Jacob OS and Jacob Core.
+It is responsible for planning, mission context, presence and future orchestration.
+"""
+
+from .brain import JacobBrain
+
+__all__ = ["JacobBrain"]
